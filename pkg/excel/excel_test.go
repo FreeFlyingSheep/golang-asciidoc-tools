bd3fa5f7a8b9df166b2f7dc3a4d6e66c4a276365
FreeFlyingSheep <chris.chenfeiyang@outlook.com>
2021-04-06 18:42:34 +0800

    excel: Add test

--- pkg/excel/excel_test.go
+++ pkg/excel/excel_test.go
@@ -1,11 +1,65 @@
 package excel
 
 import (
-	"os"
+	"bytes"
+	"strings"
 	"testing"
 )
 
 func TestInit(t *testing.T) {
-	// TODO
-	Convert(os.Stdout, "testdata/test.xlsx", "Sheet1")
+	var tests = []struct {
+		input string
+		sheet string
+		wants []string
+	}{
+		{"test.xlsx", "Sheet1",
+			[]string{
+				"[cols=\"5*1\"]",
+				"|===",
+				"|A1",
+				"|B1",
+				"|C1",
+				"|D1",
+				"|E1",
+				"2+|A2",
+				"|C2",
+				".3+|D2",
+				"|E2",
+				"|A3",
+				"|B3",
+				"|C3",
+				"|E3",
+				"|A4",
+				"|B4",
+				"|C4",
+				"|E4",
+				"|A5",
+				"2.2+|B5",
+				"|D5",
+				"|E5",
+				"|A6",
+				"|D6",
+				"|E6",
+				"|A7",
+				"|B7",
+				"|C7",
+				"|D7",
+				"|E7",
+			},
+		},
+	}
+
+	out := new(bytes.Buffer)
+	for _, test := range tests {
+		if err := Convert(out, "testdata/"+test.input, test.sheet); err != nil {
+			t.Error(err)
+		}
+
+		got := out.String()
+		for _, want := range test.wants {
+			if !strings.Contains(got, want) {
+				t.Errorf("Convert() not contains %s", want)
+			}
+		}
+	}
 }
